65f2918311df2594dd291e2e7e0d676358ee666b
“Miguel” <miguelantoliv@gmail.com>
2024-12-13 12:40:48 +0100

    refactor: Add coord fields

--- lib/domain/entities/city.dart
+++ lib/domain/entities/city.dart
@@ -2,13 +2,14 @@ class City {
   final String cityName;
   final String countryName;
   final String countryCode;
+  final double latitude;
+  final double longitude;
 
   const City({
     required this.cityName,
     required this.countryName,
     required this.countryCode,
+    required this.latitude,
+    required this.longitude,
   });
-
-  String get fullName => '$cityName, $countryName';
-  String get shortName => '$cityName, $countryCode';
 } 
